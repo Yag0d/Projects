ce98e7651b1959f3188828db40b49106dcd97b0b
Yago Augusto de Sousa Guedes <yago.augusto@dinfor.uniceub>
2018-10-31 11:42:38 -0300

    First commit

---
+++ skjoak/src/slap/Teste.java
@@ -0,0 +1,15 @@
+package slap;
+
+public class Teste {
+	
+	private String nome;
+	
+	public void setName(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getName() {
+		return nome;
+	}
+
+}
---
+++ skjoak/src/slap/sHIRIU.java
@@ -0,0 +1,20 @@
+package slap;
+
+import java.util.Scanner;
+public class sHIRIU {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		
+		Teste pessoa = new Teste();
+		
+		System.out.printf("Nome atual: %s\n", pessoa.getName());
+		System.out.println("Digite seu nome: ");
+		pessoa.setName(in.nextLine());
+		
+		System.out.printf("Novo nome: %s\n", pessoa.getName());
+		
+	}
+
+}
